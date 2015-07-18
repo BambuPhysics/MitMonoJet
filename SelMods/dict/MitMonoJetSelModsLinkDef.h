4465016654b468ee8ae05a2391d063361d4da9a6
Yutaro Iiyama <yiiyama@mit.edu>
2015-07-18 01:26:51 -0400

    Moving MonoJetAnalysisMod to MitPhysics/Skim

--- SelMods/dict/MitMonoJetSelModsLinkDef.h
+++ SelMods/dict/MitMonoJetSelModsLinkDef.h
@@ -1,10 +1,7 @@
-// $Id: MitMonoJetSelModsLinkDef.h,v 1.1 2013/06/13 03:31:18 dimatteo Exp $
-
 #ifndef MITMONOJET_SELMODS_LINKDEF_H
 #define MITMONOJET_SELMODS_LINKDEF_H
 
 #include "MitMonoJet/SelMods/interface/BoostedVAnalysisMod.h"
-#include "MitMonoJet/SelMods/interface/MonoJetAnalysisMod.h"
 #endif
  
 #ifdef __CINT__
@@ -16,6 +13,4 @@
 #pragma link C++ namespace mithep;
 
 #pragma link C++ class mithep::BoostedVAnalysisMod+;
-#pragma link C++ class mithep::ObjArray<mithep::TriggerMask>+;
-#pragma link C++ class mithep::MonoJetAnalysisMod+;
 #endif
